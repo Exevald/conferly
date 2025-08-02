cdc15f57959b028a0a3839fed03bcfb14d37d394
kirill.yashmetov <k_yashmetov@mail.ru>
2025-08-02 23:46:33 +0300

    chore: обновил конфигурацию ESLint и зависимости

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,25 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+    baseDirectory: __dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        rules: {
+            "indent": ["error", 4],
+            "semi": ["error", "never"],
+            "prefer-const": "error",
+            "no-var": "error",
+            "@typescript-eslint/consistent-type-definitions": ["error", "type"]
+        }
+    }
+]
 
-export default eslintConfig;
+export default eslintConfig
