88727102e602ef5e84e0e2df92fd22b66627b76b
kirill.yashmetov <k_yashmetov@mail.ru>
2025-08-03 09:43:56 +0300

    chore: добавил правило для переноса пропсов

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path"
-import { fileURLToPath } from "url"
-import { FlatCompat } from "@eslint/eslintrc"
+import {dirname} from "path"
+import {fileURLToPath} from "url"
+import {FlatCompat} from "@eslint/eslintrc"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -18,9 +18,11 @@ const eslintConfig = [
             "prefer-const": "error",
             "no-var": "error",
             "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-            "object-curly-spacing": ["error", "never"]
+            "object-curly-spacing": ["error", "never"],
+            "react/jsx-max-props-per-line": ["error", {"maximum": 2, "when": "multiline"}]
         }
     }
 ]
 
 export default eslintConfig
+
