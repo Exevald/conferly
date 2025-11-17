c2b3d026a9b8685222cec1d6cf82ff9e53736be9
kirill.yashmetov <k_yashmetov@mail.ru>
2025-11-17 03:05:30 +0300

    fix: сделал /prejects клиентской страницей

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {type PropsWithChildren} from 'react'
 import {ManagementSpace} from '@/widgets/projects/ManagementSpace/ManagementSpace'
 import {NavigationPanel} from '@/widgets/projects/NavigationPanel/NavigationPanel'
